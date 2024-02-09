0c192d4c7adfad103556b1b9f47bc638329b5717
zubairmoerat <zubairmoeratofficial@gmail.com>
2024-02-09 16:17:20 +0200

    expressDone

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('WELCOME TO EXPRESS');
+// HTTP Methods
+// GET
+// POST
+// PATCH
+// PUT
+// DELETE
+
+import Express from "express";
+
+const app = Express()
+const port = +process.env.PORT || 4000
+
+app.get('/', (req, res)=> {
+    res.json({
+        status: res.statusCode,
+        msg: "You're Home"
+    })
+})
+app.get('/about', (req, res)=> {
+    res.json({
+        status: res.statusCode,
+        msg: "You're on the About Page"
+    })
+})
+app.all('*', (req, res)=>{
+    res.json({
+        status: 404,
+        msg: "404 PAGE"
+    })
+})
+app.listen(port)
